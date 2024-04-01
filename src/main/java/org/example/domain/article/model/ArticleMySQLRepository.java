a37db5fb704b1dc1e6450e3af2fabb1e6f859876
chacha86 <taejincha0619@gmail.com>
2024-04-01 09:46:52 +0900

    search complete

--- src/main/java/org/example/domain/article/model/ArticleMySQLRepository.java
+++ src/main/java/org/example/domain/article/model/ArticleMySQLRepository.java
@@ -18,7 +18,37 @@ public class ArticleMySQLRepository implements Repository {
 
     @Override
     public ArrayList<Article> findArticleByKeyword(String keyword) {
-        return null;
+        String keywordWithWildCard = "%" + keyword + "%";
+        String sql = "SELECT id, title, body, hit, regDate FROM article WHERE title LIKE ?";
+
+        ArrayList<Article> articleList = new ArrayList<>();
+        try (
+                Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+        ) {
+            // 쿼리 파라미터 설정
+            pstmt.setString(1, keywordWithWildCard);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+
+                while (rs.next()) {
+                    // ResultSet에서 데이터 추출
+                    int articleId = rs.getInt("id");
+                    String title = rs.getString("title");
+                    String body = rs.getString("body");
+                    int hit = rs.getInt("hit");
+                    String regDate = rs.getString("regDate");
+
+                    // Article 객체 생성 및 반환
+                    Article article = new Article(articleId, title, body, hit, regDate);
+                    articleList.add(article);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("데이터 조회 중 오류가 발생했습니다: " + e.getMessage());
+        }
+        return articleList;
     }
 
     @Override
